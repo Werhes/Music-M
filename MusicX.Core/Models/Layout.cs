2f6e3b5c0cba8868864d48eeae6d4a59ad703422
Lyosha Shapchits <a65768795@gmail.com>
2026-08-25 12:16:58 +0300

    пофиксил discord rpc

---
+++ MusicX.Core/Models/Layout.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace MusicX.Core.Models
+{
+    public class Layout
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("owner_id")]
+        public int OwnerId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("is_editable")]
+        public int? IsEditable { get; set; }
+
+        [JsonProperty("top_title")]
+        public TopTitle TopTitle { get; set; }
+
+        [JsonProperty("subtitle")]
+        public string Subtitle { get; set; }
+        [JsonProperty("style")]
+        public string Style { get; set; }
+    }
+}
---
+++ MusicX.Server/Shared/Extensions/HubBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MusicX.Server.Shared.ListenTogether;
+using MusicX.Server.Shared.Player;
+using SignalR.Protobuf;
+
+namespace MusicX.Server.Shared.Extensions;
+
+public static class HubBuilderExtensions
+{
+    public static T AddProtobufProtocol<T>(this T builder) where T : ISignalRBuilder
+    {
+        return builder.AddProtobufProtocol(new Dictionary<int, Type>
+        {
+            [0] = typeof(PlaylistTrack),
+            [1] = typeof(SessionId),
+            [2] = typeof(PlayState),
+            [3] = typeof(ErrorState),
+            [4] = typeof(User),
+        });
+    }
+}
